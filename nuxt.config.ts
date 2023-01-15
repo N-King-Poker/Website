7da9baa82f3b3589ffcd71ad2c0a6a9d0c87b4f1
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2023-01-15 22:49:48 +0100

    :rotating_light: Fix linter errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
