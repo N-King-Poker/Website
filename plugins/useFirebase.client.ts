ab4a811d97e8dc24454d01046320381e735d294e
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2023-01-16 00:03:48 +0100

    :sparkles: Add base auth

--- plugins/useFirebase.client.ts
+++ plugins/useFirebase.client.ts
@@ -1,13 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from '@firebase/analytics'
 import { defineNuxtPlugin } from '#app'
+import { getAuth } from '@firebase/auth'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
 
   const firebaseApp = initializeApp(config.firebase)
   const analytics = getAnalytics(firebaseApp)
+  const auth = getAuth(firebaseApp)
 
   nuxtApp.provide('firebaseApp', firebaseApp)
   nuxtApp.provide('analytics', analytics)
+  nuxtApp.provide('auth', auth)
 })
