aebd9558a58eb80fab8eec03ab05646f082abacd
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2023-01-16 17:52:19 +0100

    :truck: Refactor last move of secrets

--- plugins/useFirebase.client.ts
+++ plugins/useFirebase.client.ts
@@ -6,11 +6,24 @@ import { getAuth } from '@firebase/auth'
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
 
-  const firebaseApp = initializeApp(config.firebase)
+  const configObject = {
+    apiKey: config.firebaseApiKey,
+    authDomain: config.firebaseAuthDomain,
+    projectId: config.firebaseProjectId,
+    storageBucket: config.firebaseStorageBucket,
+    messagingSenderId: config.firebaseMessagingSenderId,
+    appId: config.firebaseAppId,
+    measurementId: config.firebaseMeasurementId
+  }
+
+  const firebaseApp = initializeApp(configObject)
   const analytics = getAnalytics(firebaseApp)
   const auth = getAuth(firebaseApp)
 
   nuxtApp.provide('firebaseApp', firebaseApp)
   nuxtApp.provide('analytics', analytics)
   nuxtApp.provide('auth', auth)
+  nuxtApp.vueApp.provide('firebaseApp', firebaseApp)
+  nuxtApp.vueApp.provide('analytics', analytics)
+  nuxtApp.vueApp.provide('auth', auth)
 })
