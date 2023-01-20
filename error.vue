f6dca8c028eb2763eb61a2d5d3f16630974a1dc3
landryarki <landry.gigant@epitech.eu>
2023-01-20 14:00:55 +0100

    :construction::goa_net: error message page, custom message and a link to the homahe page

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+      An error occurred - {{ getErrorMessage(error.statusCode) }}
+    </h1>
+    <NuxtLink to="/">
+      Home page
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: {
+    type: Object,
+    default: null
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
